8d7d9527c73f2b59abb2c109a355d489a382871e
Rodolphe Pineau <pineau@rti-zone.org>
2020-01-02 11:53:03 -0800

    Fix for macOS 64bit code signing

--- installer/build.sh
+++ installer/build.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+PACKAGE_NAME="AMCDrive_X2.pkg"
+BUNDLE_NAME="org.rti-zone.AMCDriveX2"
+
+if [ ! -z "$app_id_signature" ]; then
+    codesign -f -s "$app_id_signature" --verbose ../build/Release/libAMCDrive.dylib
+fi
+
 mkdir -p ROOT/tmp/AMCDrive_X2/
 cp "../AMCDrive.ui" ROOT/tmp/AMCDrive_X2/
 cp "../domelist AMCDrive.txt" ROOT/tmp/AMCDrive_X2/
 cp "../build/Release/libAMCDrive.dylib" ROOT/tmp/AMCDrive_X2/
 
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.AMCDrive_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 AMCDrive_X2.pkg
-pkgutil --check-signature ./AMCDrive_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
 else
-pkgbuild --root ROOT --identifier org.rti-zone.AMCDrive_X2 --scripts Scripts --version 1.0 AMCDrive_X2.pkg
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
